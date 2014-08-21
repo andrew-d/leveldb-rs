0e4871f8f5e8a25dfaf45d49263e012d410c7c35
Andrew Dunham <andrew@du.nham.ca>
2014-08-21 03:34:29 -0700

    Allow creating and putting into a database

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,8 @@
 extern crate libc;
 
 use std::ptr;
-use libc::{c_char};
+use libc::{c_char, c_uchar, c_void};
+use libc::types::os::arch::c95::size_t;
 
 use ffi::ffi as cffi;
 
@@ -23,8 +24,26 @@ pub struct LevelDBError {
     errptr: *mut c_char,
 }
 
-// TODO: drop LevelDBError
-// TODO: read LevelDBError as String
+impl LevelDBError {
+    /// Return the error as a string.
+    pub fn as_string(&self) -> String {
+        unsafe {
+            std::string::raw::from_buf(self.errptr as *const u8)
+        }
+    }
+}
+
+impl std::fmt::Show for LevelDBError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::FormatError> {
+        self.as_string().fmt(f)
+    }
+}
+
+impl Drop for LevelDBError {
+    fn drop(&mut self) {
+        unsafe { cffi::leveldb_free(self.errptr as *mut c_void) };
+    }
+}
 
 pub type LevelDBResult<T> = Result<T, LevelDBError>;
 
@@ -51,7 +70,6 @@ fn with_errptr<T>(f: |*mut *mut c_char| -> T) -> LevelDBResult<T> {
     let ret = f(&mut errptr as *mut *mut c_char);
 
     if !errptr.is_null() {
-        // TODO: return error
         Err(LevelDBError {
             errptr: errptr,
         })
@@ -60,15 +78,124 @@ fn with_errptr<T>(f: |*mut *mut c_char| -> T) -> LevelDBResult<T> {
     }
 }
 
+pub fn bool_to_uchar(val: bool) -> c_uchar {
+    if val {
+        1 as c_uchar
+    } else {
+        0 as c_uchar
+    }
+}
+
+/**
+ * This structure represents options that can be used when constructing a
+ * LevelDB instance.
+ */
+pub struct DBOptions {
+    opts: *mut cffi::leveldb_options_t,
+}
+
+impl DBOptions {
+    /// Create a new Options instance
+    pub fn new() -> Option<DBOptions> {
+        let opts = unsafe { cffi::leveldb_options_create() };
+        if opts.is_null() {
+            None
+        } else {
+            Some(DBOptions {
+                opts: opts,
+            })
+        }
+    }
+
+    /**
+     * Create the database if it's missing when we try to open it.
+     */
+    pub fn create_if_missing(&mut self, val: bool) {
+        unsafe {
+            cffi::leveldb_options_set_create_if_missing(self.opts, bool_to_uchar(val));
+        }
+    }
+
+    unsafe fn ptr(&self) -> *const cffi::leveldb_options_t {
+        self.opts as *const cffi::leveldb_options_t
+    }
+}
+
+impl Drop for DBOptions {
+    fn drop(&mut self) {
+        unsafe { cffi::leveldb_options_destroy(self.opts) };
+    }
+}
+
+/**
+ * This structure represents options that can be used when writing to a LevelDB
+ * instance.
+ */
+pub struct DBWriteOptions {
+    opts: *mut cffi::leveldb_writeoptions_t,
+}
+
+impl DBWriteOptions {
+    /// Create a new DBWriteOptions instance
+    pub fn new() -> Option<DBWriteOptions> {
+        let opts = unsafe { cffi::leveldb_writeoptions_create() };
+        if opts.is_null() {
+            None
+        } else {
+            Some(DBWriteOptions {
+                opts: opts,
+            })
+        }
+    }
+
+    unsafe fn ptr(&self) -> *const cffi::leveldb_writeoptions_t {
+        self.opts as *const cffi::leveldb_writeoptions_t
+    }
+}
+
+impl Drop for DBWriteOptions {
+    fn drop(&mut self) {
+        unsafe { cffi::leveldb_writeoptions_destroy(self.opts) };
+    }
+}
+
+/**
+ * This struct represents an open instance of the database.
+ */
 pub struct DB {
     db: *mut cffi::leveldb_t,
 }
 
 impl DB {
+    /// Open a database at the given path.
     pub fn open(path: &Path) -> LevelDBResult<DB> {
+        // TODO: proper return code for OOM
+        let opts = match DBOptions::new() {
+            Some(o) => o,
+            None    => fail!("Out of memory"),
+        };
+
+        DB::open_with_opts(path, opts)
+    }
+
+    /// Create a database at the given path.
+    pub fn create(path: &Path) -> LevelDBResult<DB> {
+        // TODO: proper return code for OOM
+        let mut opts = match DBOptions::new() {
+            Some(o) => o,
+            None    => fail!("Out of memory"),
+        };
+
+        opts.create_if_missing(true);
+
+        DB::open_with_opts(path, opts)
+    }
+
+    /// Open a new database, specifying what options to use.
+    pub fn open_with_opts(path: &Path, opts: DBOptions) -> LevelDBResult<DB> {
         let res = path_as_c_str(path, |path| {
             with_errptr(|errptr| {
-                unsafe { cffi::leveldb_open(ptr::null(), path, errptr) }
+                unsafe { cffi::leveldb_open(opts.ptr(), path, errptr) }
             })
         });
 
@@ -76,10 +203,42 @@ impl DB {
             Ok(db) => db,
             Err(v) => return Err(v),
         };
-
-        Ok(DB {
+Ok(DB {
             db: db,
         })
+    }
+
+    /// Put a key into the database.
+    pub fn put(&mut self, key: &[u8], val: &[u8]) -> LevelDBResult<()> {
+        // TODO: proper return code for OOM
+        let opts = match DBWriteOptions::new() {
+            Some(o) => o,
+            None    => fail!("Out of memory"),
+        };
+
+        try!(with_errptr(|errptr| {
+            unsafe {
+                cffi::leveldb_put(
+                    self.db,
+                    opts.ptr(),
+                    key.as_ptr() as *const c_char,
+                    key.len() as size_t,
+                    val.as_ptr() as *const c_char,
+                    val.len() as size_t,
+                    errptr
+                )
+            }
+        }))
+
+        Ok(())
+    }
+}
+
+impl Drop for DB {
+    fn drop(&mut self) {
+        unsafe {
+            cffi::leveldb_close(self.db)
+        }
     }
 }
 
@@ -87,6 +246,10 @@ impl DB {
 #[cfg(test)]
 mod tests {
     extern crate test;
+
+    use std::io::TempDir;
+
+    use super::DB;
     use super::ffi::ffi;
 
     #[test]
@@ -96,5 +259,18 @@ mod tests {
 
         assert!(major_ver >= 1);
         assert!(minor_ver >= 0);
+    }
+
+    #[test]
+    fn test_can_create() {
+        let tdir = match TempDir::new("create") {
+            Some(t) => t,
+            None    => fail!("Error creating temp dir"),
+        };
+
+        let _db = match DB::create(tdir.path()) {
+            Ok(db)   => db,
+            Err(why) => fail!("Error creating DB: {}", why),
+        };
     }
 }
